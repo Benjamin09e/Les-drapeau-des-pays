92dba9fecb0909b8f2e5bbfd084bae4717ad620c
EKIA Benjamin <ekiabenjamin@gmail.com>
2022-04-08 22:56:17 +0200

    affichage des drapeaux des pays en react

---
+++ src/pages/About.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import Logo from '../components/Logo';
+import Navigation from '../components/Navigation';
+
+const About = () => {
+    return (
+        <div>
+            <Logo/>
+            <Navigation />
+            <h1>A propos</h1>
+            <br />
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+                Aspernatur quaerat in facilis impedit quas 
+                explicabo alias vero sint veritatis dolorum cupiditate quisquam
+                eius assumenda illum consequatur nobis, incidunt ducimus placeat 
+                blanditiis voluptatibus quis cum harum. Qui mollitia, molestiae, 
+                iure sint ab similique perferendis quia hic expedita totam 
+                laudantium culpa nesciunt ullam quaerat placeat quae fugit 
+                provident obcaecati quo neque porro distinctio eligendi soluta 
+                doloremque. Amet corrupti quod ex repellat, sit obcaecati 
+                similique asperiores dolores est ut officiis aliquam eveniet
+                reprehenderit ea ullam nulla delectus quasi eligendi 
+                laudantium earum iure soluta. Quidem ducimus nemo hic 
+                odio sit doloribus error mollitia ea!
+            </p>
+            <br />
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+                Aspernatur quaerat in facilis impedit quas 
+                explicabo alias vero sint veritatis dolorum cupiditate quisquam
+                eius assumenda illum consequatur nobis, incidunt ducimus placeat 
+                blanditiis voluptatibus quis cum harum. Qui mollitia, molestiae, 
+                iure sint ab similique perferendis quia hic expedita totam 
+                laudantium culpa nesciunt ullam quaerat placeat quae fugit 
+                provident obcaecati quo neque porro distinctio eligendi soluta 
+                doloremque. Amet corrupti quod ex repellat, sit obcaecati 
+                similique asperiores dolores est ut officiis aliquam eveniet
+                reprehenderit ea ullam nulla delectus quasi eligendi 
+                laudantium earum iure soluta. Quidem ducimus nemo hic 
+                odio sit doloribus error mollitia ea!
+            </p>
+        </div>
+    );
+};
+
+export default About;
